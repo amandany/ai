103342b6fd1677e5def90f8abf04d2716606f624
amandany <dan.aman@yandex.ru>
2025-10-28 22:13:31 +0300

    init

--- src/components/LetterGlitch.tsx
+++ src/components/LetterGlitch.tsx
@@ -194,7 +194,7 @@ const LetterGlitch = ({
     resizeCanvas();
     animate();
 
-    let resizeTimeout: NodeJS.Timeout;
+    let resizeTimeout: number;
 
     const handleResize = () => {
       clearTimeout(resizeTimeout);
